671da8dde914bb10cae891b9a1903fe5b1d1f0e3
Gabriel Leal <gabriellealdeoliveira2005@gmail.com>
2025-12-22 20:11:17 -0300

    inicio do projeto agendamentos Mobile, backend Java Spring

---
+++ src/main/java/com/agendamentosMobile/agendamentosMobile/AgendamentosMobileApplication.java
@@ -0,0 +1,13 @@
+package com.agendamentosMobile.agendamentosMobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendamentosMobile/agendamentosMobile/AgendamentosMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendamentosMobile.agendamentosMobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
